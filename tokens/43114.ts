5c03deec6c041a086b4f248727d802e98ff9e13b
MejiasDev <josemejiasji@gmail.com>
2023-12-28 03:54:13 +0100

    feat(vapordex): whitelist bawls DEV-1234

--- tokens/43114.ts
+++ tokens/43114.ts
@@ -1,5 +1,13 @@
 export const AvalancheTokens = [
   {
+    address: '0x2dA8312e2C08B79104c6B18bA26Bc7065ABEC704',
+    chainId: 43_114,
+    decimals: 18,
+    'https://raw.githubusercontent.com/VaporFi/VaporDEX-tokenlists/main/tokens/assets/43114/0x2dA8312e2C08B79104c6B18bA26Bc7065ABEC704/logo.png',
+    name: 'Bawls Onu',
+    symbol: 'BAWLS',
+  },
+  {
     address: '0xA4F282457980856c36380174283d2CB278eA2B37',
     decimals: 18,
     name: 'RAIN',
